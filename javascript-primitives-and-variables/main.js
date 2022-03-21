48be09c10bf158aebc31bbcc419ed161cf498ed8
avallonsmith <avallonsmith@gmail.com>
2022-03-21 17:55:11 +0000

    completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Avallon W. Graham - Smith';
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var isCool = false;
+console.log('value of isCool:', isCool);
+console.log('typeof isCool:', typeof isCool);
+
+var totalPets = 9;
+console.log('value of totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
