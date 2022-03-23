3bdb5242ecab41d4b6e7ab4230f7dba3e1c4ca09
avallonsmith <avallonsmith@gmail.com>
2022-03-23 18:38:20 +0000

    completed javascript-methods

---
+++ javascript-methods/main.js
@@ -0,0 +1,52 @@
+var firstNumber = 1;
+var secondNumber = 2;
+var thirdNumber = 3;
+var maximumValue = Math.max(firstNumber, secondNumber, thirdNumber);
+console.log('value of maximumValue:', maximumValue);
+
+var heroes = ['Spiderman', 'Wolverine', 'Hulk', 'Batman'];
+var randomNumber = Math.random() * heroes.length;
+var randomIndex = Math.floor(randomNumber);
+console.log('value of randomIdex:', randomIndex);
+
+var randomHero = heroes[randomIndex];
+console.log('value of randomHero:', randomHero);
+
+var library = [
+  {
+    title: 'Flowers For Algernon',
+    author: 'Daniel Keyes'
+  },
+  {
+    title: 'The Divine Comedy',
+    author: 'Dante Alighieri'
+  }
+];
+
+var lastBook = library.pop();
+console.log('value of lastBook:', lastBook);
+
+var firstBook = library.shift();
+console.log('value of firstBook:', firstBook);
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1);
+console.log('value of library:', library);
+
+var fullName = 'Avallon Smith';
+var firstAndLastName = fullName.split(' ');
+console.log('value of firstAndLastName:', firstAndLastName);
+
+var firstName = firstAndLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log('Say My Name:', sayMyName);
