fb70c4dc57e9f29bd8626229f4397c9ddcfcaa66
avallonsmith <avallonsmith@gmail.com>
2022-03-23 17:30:50 +0000

    incomplete, trying to fix problems

---
+++ javascript-functions-practice/get-average-of-three-numbers.js
@@ -0,0 +1,5 @@
+/* exported getAverageOfThreeNumbers */
+function getAverageOfThreeNumbers(x, y, z) {
+  var sum = (x + y + z) / 3;
+  return sum;
+}
---
+++ javascript-functions-practice/get-square-of-number.js
@@ -0,0 +1,5 @@
+/* exported getSquareOfNumber */
+function getSquareOfNumber(number) {
+  var sum = number ** 2;
+  return sum;
+}
