b48978ba472e3e0622e174ea89806d688cc28c6c
avallonsmith <avallonsmith@gmail.com>
2022-04-01 00:02:10 +0000

    incomplete

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,13 @@
+var $tabContainer = document.querySelector('.tab-container');
+//* var $nodeTab = document.querySelectorAll('.tab');
+//* var $nodeView = document.querySelectorAll('.view');
+
+function callBack(event) {
+//  console.log('event.target:', event.target);
+  if (event.target.matches('.tab')) {
+    //*    for () {
+
+  }
+}
+
+$tabContainer.addEventListener('click', callBack);
