59f224f117ce9d853e1bb32bc90030aaa32e7091
avallonsmith <avallonsmith@gmail.com>
2022-04-10 22:26:33 +0000

    update: pseudocode has been added

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -4,3 +4,9 @@ function last(array) {
   var lastArray = array[$last];
   return lastArray;
 }
+
+// create a new function namned 'rush'
+// give it one parameter 'array'
+// declare a new variable '$last' and assign it a value of the array's length minus one to get last element in the array
+// delcare a new variable 'lastArray' and assign it a value of the array at the last index
+// return the varible 'lastArray'
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -6,3 +6,10 @@ function tail(array) {
   }
   return $tail;
 }
+
+// create a new function named 'tail'
+// give it one parameter 'array'
+// delcare a new variavle '$tail" and assign it the value of an empty array
+// use a for loop to make the array contain all elements after the first
+// push the index of the array into the new array variable '$tail'
+// return the variable '$tail"
