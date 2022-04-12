ccf066b679a1438d9ae5675c17dabd5da9ec1f7d
avallonsmith <avallonsmith@gmail.com>
2022-04-12 21:22:49 +0000

    completed javascript-ajax exercise

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+var $userList = document.querySelector('#user-list');
+
+function request(user) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    for (var i = 0; i < xhr.response.length; i++) {
+      var $li = document.createElement('li');
+      $li.textContent = xhr.response[i].name;
+      $userList.appendChild($li);
+    }
+  });
+  xhr.send();
+}
+request(name);
