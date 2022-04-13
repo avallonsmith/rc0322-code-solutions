90a02cf06e68c62e8bb52d9147b3b687a75b7e07
avallonsmith <avallonsmith@gmail.com>
2022-04-13 22:27:41 +0000

    completed javascript-bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -5,12 +5,38 @@ function Account(number, holder) {
   this.transactions = [];
 }
 
-// Account.prototype.deposit = function (amount) {
-//  var
-//  return
-// };
+Account.prototype.deposit = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var transaction = new Transaction('deposit', amount);
+    this.transactions.push(transaction);
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Account.prototype.withdrawl = function (amount) {
-// var
-//  return
-// };
+Account.prototype.withdraw = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var transactions = new Transaction('withdrawal', amount);
+    this.transactions.push(transactions);
+    return true;
+  } else {
+    return false;
+  }
+};
+Account.prototype.getBalance = function () {
+  var total = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      total += this.transactions[i].amount;
+    }
+    if (this.transactions[i].type === 'withdrawal') {
+      total -= this.transactions[i].amount;
+    }
+  }
+  if (this.transactions.length === 0) {
+    return 0;
+  } else {
+    return total;
+  }
+};
