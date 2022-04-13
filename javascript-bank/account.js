b4d93e1922eb9e4dd1e94d00f418d807ef6be465
avallonsmith <avallonsmith@gmail.com>
2022-04-13 16:58:51 +0000

    incomplete

---
+++ javascript-bank/account.js
@@ -0,0 +1,16 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+// Account.prototype.deposit = function (amount) {
+//  var
+//  return
+// };
+
+// Account.prototype.withdrawl = function (amount) {
+// var
+//  return
+// };
