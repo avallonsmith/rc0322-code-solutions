abba1fb81e14669c3c97743be8bcc64dcd7e7f31
avallonsmith <avallonsmith@gmail.com>
2022-05-04 01:19:37 +0000

    completed node-module-system exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('type of exports:', typeof exports);
+console.log('exports:', exports);
+
+console.log('type of require:', typeof require);
+console.log('require:', require);
+
+console.log('type of module:', typeof module);
+console.log('modue:', module);
+
+console.log('type of __filename:', typeof __filename);
+console.log('__filename:', __filename);
+
+console.log('type of __dirname:', typeof __dirname);
+console.log('__dirname:', __dirname);
